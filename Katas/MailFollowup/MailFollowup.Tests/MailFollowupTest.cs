58b301eaa6ca31c7ef94588097204da392381ff5
Lasse Clausen <lasse.clausen@sinc.de>
2026-08-27 14:10:21 +0200

    feat: mail followup Kata

---
+++ Katas/MailFollowup/MailFollowup.Tests/MailFollowupTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace MailFollowup.Tests;
+
+public class MailFollowupTest
+{
+    // 02.04.2026 10:30
+    private static DateTime _test = new (2026,2,4,10,30,0);
+
+    public static TheoryData<string, DateTime> MailFollupTests = new()
+    {
+        { "7days@followup.cc", _test.AddDays(7) },
+        { "12hours@followup.cc", _test.AddHours(12) },
+        { "aug15-9am@followup.cc", new(2026, 8, 15, 9, 0, 0) },
+        { "1week3days5hours@followup.cc", _test.AddDays(10).AddHours(5) },
+    };
+        
+    [Theory, MemberData(nameof(MailFollupTests))]
+    public void TestFollowupZeitpunkt(string followupMail, DateTime expected)
+    {
+        DateTime result = Program.FollowupZeitpunkt(_test, followupMail);
+        Assert.Equal(expected, result);
+    }
+}
--- Katas/MailFollowup/MailFollowup/Program.cs
+++ Katas/MailFollowup/MailFollowup/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Text.RegularExpressions;
+// ReSharper disable InconsistentNaming
+
 namespace MailFollowup;
 
 /// <summary>
@@ -6,10 +9,104 @@ namespace MailFollowup;
 ///     Aufgabenbeschreibung: siehe README.md in diesem Projekt.
 ///     Quelle: https://ccd-school.de/coding-dojo/function-katas/mail-followup/
 /// </summary>
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    private enum Months
     {
-        Console.WriteLine("Mail Followup");
+        jan = 1,
+        feb = 2,
+        mar = 3,
+        apr = 4,
+        may = 5,
+        jun = 6,
+        jul = 7,
+        aug = 8,
+        sep = 9,
+        oct = 10,
+        nov = 11,
+        dec = 12,
+    }
+
+    public static DateTime FollowupZeitpunkt(DateTime now, string emailadresse)
+    {
+        DateTime followupZeitpunkt = now;
+        string timeStuff = emailadresse.Split('@')[0];
+        string generalTimeW = "([0-9]+)week|weeks";
+        string generalTimeD = "([0-9]+)day|days";
+        string generalTimeH = "([0-9]+)hour|hours";
+        string generalTimeM = "([0-9]+)minute|minutes";
+        string specificTimeM = $"({string.Join('|', Enum.GetNames<Months>())})([0-9]+)";
+        string specificTimeAmPm = "([0-9]+)(am|pm)";
+        
+        Match matchResult =  Regex.Match(timeStuff, generalTimeW);
+        if (matchResult.Success)
+        {
+            int.TryParse(matchResult.Groups[1].Value, out int weeks);
+            {
+                followupZeitpunkt = followupZeitpunkt.AddDays(weeks*7);
+            }
+        }
+        
+        matchResult =  Regex.Match(timeStuff, generalTimeD);
+        if (matchResult.Success)
+        {
+            int.TryParse(matchResult.Groups[1].Value, out int days);
+            {
+                followupZeitpunkt = followupZeitpunkt.AddDays(days);
+            }
+        }
+        
+        matchResult =  Regex.Match(timeStuff, generalTimeH);
+        if (matchResult.Success)
+        {
+            int.TryParse(matchResult.Groups[1].Value, out int hours);
+            {
+                followupZeitpunkt = followupZeitpunkt.AddHours(hours);
+            }
+        }
+        
+        matchResult =  Regex.Match(timeStuff, generalTimeM);
+        if (matchResult.Success)
+        {
+            int.TryParse(matchResult.Groups[1].Value, out int minutes);
+            {
+                followupZeitpunkt = followupZeitpunkt.AddMinutes(minutes);
+            }
+        }
+        
+        matchResult =  Regex.Match(timeStuff, specificTimeM);
+        if (matchResult.Success)
+        {
+            int day = followupZeitpunkt.Day;
+            
+            if(int.TryParse(matchResult.Groups[2].Value, out int days))
+            {
+                day = days;
+            }
+            
+            if (Enum.TryParse(matchResult.Groups[1].Value, out Months months))
+            {
+                followupZeitpunkt = new  DateTime(followupZeitpunkt.Year, (int) months, day, followupZeitpunkt.Hour, followupZeitpunkt.Minute, followupZeitpunkt.Second);
+            }
+        }
+        
+        matchResult =  Regex.Match(timeStuff, specificTimeAmPm);
+        if (matchResult.Success)
+        {
+            string amPm = matchResult.Groups[2].Value;
+            int.TryParse(matchResult.Groups[1].Value, out int oClock);
+            {
+                if (string.Equals("AM", amPm, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    followupZeitpunkt = new  DateTime(followupZeitpunkt.Year, followupZeitpunkt.Month, followupZeitpunkt.Day, oClock, 0, 00);
+                }
+                else
+                {
+                    followupZeitpunkt = new  DateTime(followupZeitpunkt.Year, followupZeitpunkt.Month, followupZeitpunkt.Day, 12 + oClock, 0, 00);
+                }
+            }
+        }
+        
+        return followupZeitpunkt;
     }
 }
