972723d4a5af6c952d9949b7ce80544c9c075301
Lasse Clausen <lasse.clausen@sinc.de>
2026-08-27 10:36:19 +0200

    feat: tannenbaum Kata

---
+++ Katas/Tannenbaum/Tannenbaum.Tests/TannebaumTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Tannenbaum.Tests;
+
+public class TannebaumTest
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+ 
+    public TannebaumTest(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        //Der Testoutput hat die ersten leerzeichen weg ge-trimmed, daher noch ein text vorne dran, dass das nicht passiert.
+        _testOutputHelper.WriteLine("Gurke:\n\r"+string.Join("\n\r", Program.Tannenbaum(5)));
+    }
+}
--- Katas/Tannenbaum/Tannenbaum/Program.cs
+++ Katas/Tannenbaum/Tannenbaum/Program.cs
@@ -6,10 +6,41 @@ namespace Tannenbaum;
 ///     Aufgabenbeschreibung: siehe README.md in diesem Projekt.
 ///     Quelle: https://ccd-school.de/coding-dojo/function-katas/tannenbaum/
 /// </summary>
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Tannenbaum");
+        Console.WriteLine(string.Join("\n\r", Tannenbaum(5)));
+    }
+
+    public static string[] Tannenbaum(int length)
+    {
+        string[] result = new string[length+1];
+
+        for (int i = 1; i <= length; i++)
+        {
+            string line = string.Empty;
+            for (int j = 1; j <= (i+i-1); j++)
+            {
+                //Fand die Idee witzig, dass Random Christbaumkugeln auftauchen
+                int randomInt = Random.Shared.Next(1, 100);
+                if(randomInt > 95 || i==1)
+                    line += "*";
+                else
+                    line += "X";
+            }
+            result[i-1] = line;
+        }
+        
+        result[length] = "I";
+        
+        //Length -1 da der Stamm ja eine andere Einrückung erhält.
+        for (int i = 0; i < result.Length-1; i++)
+        {
+            result[i] = result[i].PadLeft(length+i, '\u00A0');
+        }
+        result[length] = result[length].PadLeft(length, '\u00A0');
+        
+        return result;
     }
 }
